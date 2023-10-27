9693452e95f2864c2148f2538e5180f153e47e9a
Kanur <ergo.proxy.ec@gmail.com>
2023-10-27 18:05:05 +0300

    kk comps for Alsu

--- src/ui/OperationDateTime/OperationDateTime.jsx
+++ src/ui/OperationDateTime/OperationDateTime.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export const OperationDateTime = () => {
-  return (
-    <div>OperationDateTime</div>
-  )
-}
+  return <div>OperationDateTime</div>;
+};
--- src/ui/Scores/Scores.jsx
+++ src/ui/Scores/Scores.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export const Scores = () => {
-  return (
-    <div>Scores</div>
-  )
-}
+  return <div>Scores</div>;
+};
