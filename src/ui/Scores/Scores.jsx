40e89acc48abe0ebefba005c609561870bc2879f
AlsuLI <142306929+AlsuLI@users.noreply.github.com>
2023-10-31 03:41:07 -0700

    added Direct., Scors,InputCheckbox is not ready!!!

--- src/ui/Scores/Scores.jsx
+++ src/ui/Scores/Scores.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import s from "./Score.module.scss";
+import cn from "../../utils/cn/cn";
 
-export const Scores = () => {
+// eslint-disable-next-line react/prop-types
+export const Scores = ({ condition }) => {
   return (
-    <div className={s.score__bad__wrapper}>
-      <span className={s.score__bad}>Плохо</span>
+    <div
+      className={cn(
+      s.score__bad__wrapper,
+        {
+          [s.score__good__wrapper]: condition === "Хорошо",
+          [s.score__perfect__wrapper]: condition === "Отлично",
+          [s.score__bad__wrapper]: condition === "Плохо",
+        },
+        []
+      )}
+    >
+      <span className={s.score}>{condition}</span>
     </div>
   );
 };
