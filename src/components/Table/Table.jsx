f1839db1af9bd4d5b6869a2175c3e8caa1f9b1f6
AlsuLI <142306929+AlsuLI@users.noreply.github.com>
2023-11-05 11:05:16 -0800

    api maping

---
+++ src/components/Table/Table.jsx
@@ -0,0 +1,36 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+import s from "./Table.module.scss";
+import { Arrows } from "../../ui/Arrows/Arrows";
+
+export const Table = ({ check, key, call, date, avatar, number, source, time }) => {
+  function time_convert(num) {
+    var min = Math.floor(num / 60);
+    var sec = num % 60;
+    if (String(sec).length < 2) {
+      sec = "0" + sec;
+    }
+    return min + ":" + sec;
+  }
+
+  return (
+    <tr key={key} className={s.table__rows}>
+      <td className={s.table__check}>{check}</td>
+      <td className={s.arrow}>
+        {call === 1 ? (
+          <Arrows variant={"incommingCall"} />
+        ) : (
+          <Arrows variant={"outgoingCall"} />
+        )}
+      </td>
+      <td className={s.table__date}>{date.slice(11, 16)}</td>
+      <td>
+        <img className={s.table__avatar} src={avatar} alt="" />
+      </td>
+      <td>{"+" + number}</td>
+      <td>{source}</td>
+      <td>ocenka</td>
+      <td>{time_convert(time)}</td>
+    </tr>
+  );
+};
---
+++ src/components/TableName/TableName.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+import s from "./TableName.module.scss";
+
+export const TableName = ({ children }) => {
+  return (
+    <table>
+      <thead className={s.table__head}>
+        <tr className={s.table__head}>
+          <th>
+            <svg
+              width="16"
+              height="16"
+              viewBox="0 0 16 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g id="Property 1=unchecked &#226;&#128;&#148; default">
+                <path
+                  id="Vector"
+                  d="M14.2222 1.77778V14.2222H1.77778V1.77778H14.2222ZM14.2222 0H1.77778C0.8 0 0 0.8 0 1.77778V14.2222C0 15.2 0.8 16 1.77778 16H14.2222C15.2 16 16 15.2 16 14.2222V1.77778C16 0.8 15.2 0 14.2222 0Z"
+                  fill="#ADBFDF"
+                />
+              </g>
+            </svg>
+          </th>
+          <th >Тип</th>
+          <th >Время</th>
+          <th >Сотрудник</th>
+          <th >Звонок</th>
+          <th>Источник</th>
+          <th>Оценка</th>
+          <th>Длительность</th>
+        </tr>
+      </thead>
+      <tbody>{children}</tbody>
+    </table>
+  );
+};
